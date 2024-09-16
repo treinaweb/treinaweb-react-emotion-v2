61e92eed6c67efcec217d9343c8e0d62ea8a16af
Wesley Gado <wesleygado@gmail.com>
2024-09-16 16:53:16 -0300

    04 - Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "@emotion/styled"
+import { useState } from "react";
+
+const MinhaDiv = styled('div')`
+  background-color: ${({ ligado }) => (ligado ? 'white' : 'red')};
+  color: blue;
+  font-size: 50px;
+`;
+
+const OutraDiv = styled(MinhaDiv)`
+  padding: 25px;
+  border: 10px solid black;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [ligado, setLigado] = useState(true);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div onClick={() => setLigado(!ligado)}>
+      <MinhaDiv ligado={ligado}>Treina Web</MinhaDiv>
+    </div>
   )
 }
 
