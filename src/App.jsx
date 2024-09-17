85b11500c96ea9f10f88d6dac592006739a176c5
Wesley Gado <wesleygado@gmail.com>
2024-09-17 16:50:50 -0300

    08 - Temas

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,33 @@
-import { keyframes } from "@emotion/react";
 import styled from "@emotion/styled/macro";
+import { ThemeProvider } from "@emotion/react";
 
 const MeuSpan = styled('span')``;
 
-const sumir = keyframes`
-  0%{
-    opacity: 1;
-  }
+const dark = {
+  corDaFonte: 'white',
+  corDeFundo: 'black',
+}
 
-  100%{
-    opacity: 0;
-  }
-`;
+const light = {
+  corDaFonte: 'black',
+  corDeFundo: 'white',
+}
 
 const MinhaDiv = styled('div')`
-  background-color: white;
-  color: blue;
+  background-color: ${(props) => props.theme.corDeFundo};
+  color: ${(props) => props.theme.corDaFonte};
   font-size: 50px;
-
-  &:hover ${MeuSpan} {
-    color: red;
-    animation: ${sumir} 1s ease alternate-reverse infinite;
-    @media (max-width: 600px) {
-      color: black;
-    }
-  }
 `;
 
 function App() {
   return (
-    <div>
-      <MinhaDiv>Treina Web<br />
-        <MeuSpan>Plataforma para desenvolvedores...</MeuSpan>
-      </MinhaDiv>
-
-    </div>
+    <ThemeProvider theme={dark}>
+      <div>
+        <MinhaDiv>Treina Web<br />
+          <MeuSpan>Plataforma para desenvolvedores...</MeuSpan>
+        </MinhaDiv>
+      </div >
+    </ThemeProvider >
   )
 }
 
